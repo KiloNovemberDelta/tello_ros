c88dd7abe79f3bfd9a578690a6433867520137c7
jamy <jamychahal@hotmail.com>
2021-10-15 12:29:30 +0200

    Add CV bridge

--- tello_controller/aruco_focus.py
+++ tello_controller/aruco_focus.py
@@ -1,9 +1,15 @@
+import time
+
 import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
 
 from tello_msgs.srv import TelloAction
+from rclpy.qos import qos_profile_sensor_data
+from cv_bridge import CvBridge  # Package to convert between ROS and OpenCV Images
+import cv2  # OpenCV library
 
 
 class ActionManager(Node):
@@ -33,19 +39,33 @@ class Controller(Node):
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.cmd_vel_loop)
         # Init desired speed
-        self.vx = 0.01
+        self.vx = 0.0
         self.vy = 0.0
         self.v_yaw = 0.0
 
+        ### CREATE THE SUBSCRIBER FOR IMAGE RECEPTION
+        self.image_subscription_ = self.create_subscription(Image, '/drone1/image_raw', self.image_callback, 10)
+        # Used to convert between ROS and OpenCV images
+        self.br = CvBridge()
+
     def cmd_vel_loop(self):
         """
-        I'm used to publish every 0.5 seconds the velocity command
+        I'm used to publish every 0.5 seconds the velocity command.
+        You don't need to modify me, just modify vx, vy and v_yaw when you need it.
         """
         msg = Twist()
         msg.linear.x = self.vx
         msg.linear.y = self.vy
         msg.angular.z = self.v_yaw
         self.cmd_vel_publisher_.publish(msg)
+
+    def image_callback(self, data):
+        self.get_logger().info('Receiving video frame')
+        current_frame = self.br.imgmsg_to_cv2(data)
+        # Display image
+        cv2.imshow("camera", current_frame)
+
+        cv2.waitKey(1)
 
 
 def main(args=None):
@@ -56,16 +76,16 @@ def main(args=None):
     action_manager.ask_for_takeoff()
     ready_to_continue_mission = False
 
-    #Try to takeoff, wait for the return of the service
+    # Try to takeoff, wait for the return of the service
     while rclpy.ok():
         rclpy.spin_once(action_manager)
         if action_manager.future.done():
             try:
                 response = action_manager.future.result()
-                if response.rc == 1: #OK
+                if response.rc == 1:  # OK
                     print("Take off is a success !")
                     ready_to_continue_mission = True
-                else: #NOT OK
+                else:  # NOT OK
                     print("Something is wrong with the takeoff. LANDING NOW")
             except Exception as e:
                 action_manager.get_logger().info("Service call failed %r" % (e,))
@@ -87,7 +107,7 @@ def main(args=None):
         if action_manager.future.done():
             try:
                 response = action_manager.future.result()
-                if response.rc == 1: #OK
+                if response.rc == 1:  # OK
                     print("Landing is a success !")
                     break  # Only if landing is ok
             except Exception as e:
